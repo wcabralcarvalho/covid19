8cb819f8b359b69fc9bf59ba45169dfb75968eab
wcabralcarvalho <wcabral.carvalho@gmail.com>
2021-12-04 16:13:57 -0300

    Novas Correções

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -itd -p 80:3000 wandersoncabral/app-covid:develop
+docker run -itd -p 80:3000 wandersoncabral/app-covid
--- scripts/stop-container.sh
+++ scripts/stop-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm -f $(docker ps -qa) || true
-docker rmi wandersoncabral/app-covid:develop || true
+docker rmi wandersoncabral/app-covid || true
